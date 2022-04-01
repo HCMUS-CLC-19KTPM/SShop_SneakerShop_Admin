92aff6b35795462ade01af1968f76876544ece07
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2022-04-01 22:50:46 +0700

    complete edit information mode in profile page

--- app/src/main/java/com/example/sshop_sneakershop_admin/Account/AccountDetailActivity.kt
+++ app/src/main/java/com/example/sshop_sneakershop_admin/Account/AccountDetailActivity.kt
@@ -1,9 +1,10 @@
 package com.example.sshop_sneakershop_admin.Account
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sshop_sneakershop_admin.R
+
 
 class AccountDetailActivity : AppCompatActivity() {
 
@@ -21,41 +22,64 @@ class AccountDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_detail)
 
-        username = findViewById(R.id.admin_account_detail_textview_username_2)
-        address = findViewById(R.id.admin_account_detail_textview_address)
-        phone = findViewById(R.id.admin_account_detail_textview_phone)
-        gender = findViewById(R.id.admin_account_detail_textview_gender)
-        birthday = findViewById(R.id.admin_account_detail_textview_birthday)
-        status = findViewById(R.id.admin_account_detail_spinner_status)
-        deleteBtn = findViewById(R.id.admin_account_detail_button_delete)
-        submitBtn = findViewById(R.id.admin_account_detail_button_confirm)
-        editProfileTV = findViewById(R.id.admin_account_detail_textview_edit_profile)
+        username = findViewById(R.id.account_detail_edittext_username_2)
+        address = findViewById(R.id.account_detail_edittext_address)
+        phone = findViewById(R.id.account_detail_edittext_phone)
+        gender = findViewById(R.id.account_detail_edittext_gender)
+        birthday = findViewById(R.id.account_detail_edittext_birthday)
+        status = findViewById(R.id.account_detail_spinner_status)
+        deleteBtn = findViewById(R.id.account_detail_button_delete)
+        submitBtn = findViewById(R.id.account_detail_button_confirm)
+        editProfileTV = findViewById(R.id.account_detail_textview_edit_profile)
 
         editProfileTV!!.setOnClickListener {
-            username!!.isFocusableInTouchMode = true
-            address!!.isFocusableInTouchMode = true
-            phone!!.isFocusableInTouchMode = true
-            gender!!.isFocusableInTouchMode=true
-            birthday!!.isFocusableInTouchMode=true
-            editProfileTV!!.text = "Editing Mode"
+            turnOnEditMode()
         }
         submitBtn!!.setOnClickListener {
-            username!!.clearFocus()
-            address!!.clearFocus()
-            phone!!.clearFocus()
-            gender!!.clearFocus()
-            birthday!!.clearFocus()
+            turnOfEditMode()
 
-            username!!.isFocusableInTouchMode = false
-            address!!.isFocusableInTouchMode = false
-            phone!!.isFocusableInTouchMode = false
-            gender!!.isFocusableInTouchMode=false
-            birthday!!.isFocusableInTouchMode=false
-            editProfileTV!!.text = "Edit Profile"
         }
 
-        deleteBtn!!.setOnClickListener{
+        deleteBtn!!.setOnClickListener {
             Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show()
         }
     }
+    fun turnOnEditMode(){
+        username!!.isFocusableInTouchMode = true
+        address!!.isFocusableInTouchMode = true
+        phone!!.isFocusableInTouchMode = true
+        gender!!.isFocusableInTouchMode = true
+        birthday!!.isFocusableInTouchMode = true
+
+        username!!.setBackgroundResource(androidx.appcompat.R.drawable.abc_edit_text_material)
+        address!!.setBackgroundResource(androidx.appcompat.R.drawable.abc_edit_text_material)
+        phone!!.setBackgroundResource(androidx.appcompat.R.drawable.abc_edit_text_material)
+        gender!!.setBackgroundResource(androidx.appcompat.R.drawable.abc_edit_text_material)
+        birthday!!.setBackgroundResource(androidx.appcompat.R.drawable.abc_edit_text_material)
+
+
+        editProfileTV!!.text = "Editing Mode"
+    }
+    fun turnOfEditMode(){
+        username!!.clearFocus()
+        address!!.clearFocus()
+        phone!!.clearFocus()
+        gender!!.clearFocus()
+        birthday!!.clearFocus()
+
+        username!!.isFocusableInTouchMode = false
+        address!!.isFocusableInTouchMode = false
+        phone!!.isFocusableInTouchMode = false
+        gender!!.isFocusableInTouchMode = false
+        birthday!!.isFocusableInTouchMode = false
+
+        username!!.background = null
+        address!!.background = null
+        phone!!.background = null
+        gender!!.background = null
+        birthday!!.background = null
+
+        editProfileTV!!.text = "Edit Profile"
+    }
 }
+
